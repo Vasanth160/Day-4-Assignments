c42dc8fcd87e134c3b5122bbcf6e7ee3e89ca0d4
Vasanth K <77053192+Vasanth160@users.noreply.github.com>
2023-02-07 17:03:53 +0530

    Delete maximum.py

--- maximum.py
+++
@@ -1,8 +0,0 @@
-# Day-4 Question num 1
-def maximum(a, b, c):
-   list = [a, b, c]
-   return max(list)
-a = int(input("Enter First number: "))
-b = int(input("Enter Second number: "))
-c = int(input("Enter Third number: "))
-print("Maximum Number is ::>",maximum(a, b, c))
